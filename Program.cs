7727965d8e6066fe7da98a8a9e5caf0dc5607007
Vinicius Rigon <viniciusrigon@gmail.com>
2023-12-11 16:41:02 -0300

    adicionado arquivo configuracao e validaçoes de numero de argumentos

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Configuration;
+
+IConfiguration Configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .AddCommandLine(args)
+    .Build();
+
+
+if (args.Length != 3)
+{
+    Console.WriteLine("Wrong number of arguments. Should be quote-alert [TICKER] [min-price] [max-price] ");
+}
+else
+{
+    string ticker = args[0];
+    decimal minprice = Convert.ToDecimal(args[1]);
+    decimal maxprice = Convert.ToDecimal(args[2]);
+
+
+
+
+
+}
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(Configuration.GetValue<string>("emaildestino"));
